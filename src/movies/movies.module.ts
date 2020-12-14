b0225fbef5c8d252622e6333972c31ec5b692f83
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 22:06:25 +0100

    Import TypegooseModule with model

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { OmdbModule } from '../omdb/omdb.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { MovieModel } from './movie.model';
 
 @Module({
+  imports: [TypegooseModule.forFeature([MovieModel]), OmdbModule],
   providers: [MoviesService],
-  controllers: [MoviesController]
+  controllers: [MoviesController],
 })
 export class MoviesModule {}
