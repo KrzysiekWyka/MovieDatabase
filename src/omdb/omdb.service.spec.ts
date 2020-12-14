0b89b690b3743e43ac2d3c57831f08013956a60a
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 22:00:24 +0100

    Create OmdbService & tests

--- src/omdb/omdb.service.spec.ts
+++ src/omdb/omdb.service.spec.ts
@@ -1,18 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OmdbService } from './omdb.service';
+import {
+  HttpModule,
+  NotFoundException,
+  Provider,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+import omdbConfig from '../omdb/omdb.config';
+import * as nock from 'nock';
 
-describe('OmdbService', () => {
-  let service: OmdbService;
+const TEST_API_KEY = 'b2d47b5c';
+
+// INFO: Tests disabled to prevent reaching daily limits
+xdescribe('OmdbService', () => {
+  let sut: OmdbService;
 
   beforeEach(async () => {
+    const fakeConfig: Provider = {
+      provide: omdbConfig.KEY,
+      useValue: {
+        apiKey: TEST_API_KEY,
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OmdbService],
+      imports: [HttpModule],
+      providers: [OmdbService, fakeConfig],
     }).compile();
 
-    service = module.get<OmdbService>(OmdbService);
+    sut = module.get<OmdbService>(OmdbService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getMovieInfoByTitle', () => {
+    it('should throws ServiceUnavailableException when api call faild', async () => {
+      const title = 'fooBar';
+
+      nock('http://www.omdbapi.com/')
+        .get('/')
+        .query({ apiKey: TEST_API_KEY, t: title, type: 'movie' })
+        .reply(500);
+
+      await expect(sut.getMovieInfoByTitle(title)).rejects.toThrow(
+        ServiceUnavailableException,
+      );
+
+      nock.restore();
+    });
+
+    it('should throws NotFoundException when movie could not be found', async () => {
+      await expect(
+        sut.getMovieInfoByTitle('notExistingMovieTitle'),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should return movie info', async () => {
+      const result = await sut.getMovieInfoByTitle('pulp fiction');
+
+      expect(result).toEqual({
+        Title: 'Pulp Fiction',
+        Year: '1994',
+        Rated: 'R',
+        Released: '14 Oct 1994',
+        Runtime: '154 min',
+        Genre: 'Crime, Drama',
+        Director: 'Quentin Tarantino',
+        Writer:
+          'Quentin Tarantino (stories), Roger Avary (stories), Quentin Tarantino',
+        Actors: 'Tim Roth, Amanda Plummer, Laura Lovelace, John Travolta',
+        Plot:
+          'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+        Language: 'English, Spanish, French',
+        Country: 'USA',
+        Awards: 'Won 1 Oscar. Another 69 wins & 75 nominations.',
+        Poster:
+          'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+        Ratings: [
+          { Source: 'Internet Movie Database', Value: '8.9/10' },
+          { Source: 'Rotten Tomatoes', Value: '92%' },
+          { Source: 'Metacritic', Value: '94/100' },
+        ],
+        Metascore: '94',
+        imdbRating: '8.9',
+        imdbVotes: '1,807,979',
+        imdbID: 'tt0110912',
+        Type: 'movie',
+        DVD: 'N/A',
+        BoxOffice: '$107,928,762',
+        Production: 'Miramax Films, A Band Apart, Jersey Films',
+        Website: 'N/A',
+        Response: 'True',
+      });
+    });
   });
 });
--- src/omdb/omdb.service.ts
+++ src/omdb/omdb.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpService,
+  Inject,
+  Injectable,
+  NotFoundException,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import omdbConfig from './omdb.config';
+
+interface OmdbResponse {
+  Response: string;
+  Title?: string;
+  Released?: string;
+  Genre?: string;
+  Directory?: string;
+  [key: string]: unknown;
+}
 
 @Injectable()
-export class OmdbService {}
+export class OmdbService {
+  constructor(
+    @Inject(omdbConfig.KEY)
+    private readonly config: ConfigType<typeof omdbConfig>,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async getMovieInfoByTitle(title: string) {
+    try {
+      const { data } = await this.httpService
+        .get<OmdbResponse>(`http://www.omdbapi.com/`, {
+          params: {
+            apiKey: this.config.apiKey,
+            t: title,
+            type: 'movie',
+          },
+        })
+        .toPromise();
+
+      if (!this.isSuccess(data.Response)) {
+        throw new NotFoundException();
+      }
+
+      return data;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      // TODO: Use logger like pino in future to log error
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  private isSuccess(responseText: string) {
+    return responseText === 'True';
+  }
+}
