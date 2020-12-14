0379337e0abc09681c16571e2706e184ed593006
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 20:11:22 +0100

    Create AuthService with tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersRepository } from '../users/users.repository';
+import { Provider } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { when } from 'jest-when';
+
+jest.mock('bcryptjs');
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let usersRepository: Partial<Record<keyof UsersRepository, jest.Mock>>;
+  let sut: AuthService;
 
   beforeEach(async () => {
+    const fakeUsersRepository: Provider = {
+      provide: UsersRepository,
+      useValue: { findOneByUsername: jest.fn() },
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, fakeUsersRepository],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    sut = module.get<AuthService>(AuthService);
+    usersRepository = module.get<UsersRepository, unknown>(UsersRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('validateUser', () => {
+    const username = 'fooBar';
+    const password = 'barFoo';
+
+    const returnedUser = {
+      _id: 'user123',
+      username: username,
+      password: 'hashedPassword',
+    };
+
+    const recordFindOneByUsername = () =>
+      when(usersRepository.findOneByUsername)
+        .calledWith(username)
+        .mockResolvedValueOnce(returnedUser);
+
+    const recordCompare = (isCorrect: boolean) => {
+      const compareMock = bcrypt.compare as jest.Mock;
+
+      compareMock.mockResolvedValueOnce(isCorrect);
+
+      return compareMock;
+    };
+
+    it('should return undefined when specified user could not be found', async () => {
+      const result = await sut.validateUser(username, password);
+
+      expect(result).toBeUndefined();
+      expect(usersRepository.findOneByUsername).toHaveBeenCalledWith(username);
+      expect(bcrypt.compare).not.toHaveBeenCalled();
+    });
+
+    it('should return undefined when specified user provided wrong password', async () => {
+      recordFindOneByUsername();
+
+      const compareMock = recordCompare(false);
+
+      const result = await sut.validateUser(username, password);
+
+      expect(result).toBeUndefined();
+      expect(compareMock).toHaveBeenCalledWith(password, returnedUser.password);
+    });
+
+    it('should return user without password when provided password is correct', async () => {
+      recordFindOneByUsername();
+
+      const compareMock = recordCompare(true);
+
+      const result = await sut.validateUser(username, password);
+
+      const { password: _password, ...restUser } = returnedUser;
+
+      expect(result).toEqual(restUser);
+      expect(compareMock).toHaveBeenCalledWith(password, returnedUser.password);
+    });
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { UsersRepository } from '../users/users.repository';
+import * as _ from 'lodash';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersRepository.findOneByUsername(username);
+
+    if (_.isEmpty(user)) {
+      return undefined;
+    }
+
+    const isPasswordCorrect = await this.checkPassword(password, user.password);
+
+    return !isPasswordCorrect ? undefined : _.omit(user, 'password');
+  }
+
+  private checkPassword(plainPassword: string, hashedPassword: string) {
+    return bcrypt.compare(plainPassword, hashedPassword);
+  }
+}
