d3ab1c4005c4a0ac41a25990b9541b3597ad78e5
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 18:57:54 +0100

    Import ConfigModule & database provider in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { databaseProvider } from './database/database.provider';
+
+// TODO: Add config validation
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), databaseProvider],
   controllers: [AppController],
   providers: [AppService],
 })
