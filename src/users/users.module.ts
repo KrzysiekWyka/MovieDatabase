bc01d129683d109b0805c0c2291af69118564b10
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 19:32:39 +0100

    Define UsersRepository as UsersModule provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [TypegooseModule.forFeature([UserModel])],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService]
 })
 export class UsersModule {}
