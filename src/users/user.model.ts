a772c3c1fe7d861ab4faeab182b0f657a3cf6769
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 20:59:00 +0100

    Add internalId & name properties to UserModel

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,26 +1,44 @@
 import { BaseModel } from '../common/base.model';
 import { modelOptions, prop } from '@typegoose/typegoose';
 import { UserPlan } from './user-plan.enum';
-import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-@modelOptions({schemaOptions: {timestamps: true, collection: 'users'}})
-export class UserModel extends BaseModel{
+@modelOptions({ schemaOptions: { timestamps: true, collection: 'users' } })
+export class UserModel extends BaseModel {
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty({ type: 'integer' })
+  @prop({ required: true })
+  internalId!: number;
+
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @prop({required: true, unique: true})
+  @prop({ required: true, unique: true })
   username!: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @prop({required: true})
+  @prop({ required: true })
   password!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @prop({ required: true })
+  name!: string;
 
   @IsEnum(UserPlan)
   @IsOptional()
-  @ApiProperty({enum: UserPlan, default: UserPlan.BASIC})
-  @prop({required: true, default: UserPlan.BASIC})
-  plan!: UserPlan
+  @ApiProperty({ enum: UserPlan, default: UserPlan.BASIC })
+  @prop({ required: false, default: UserPlan.BASIC })
+  plan!: UserPlan;
 }
