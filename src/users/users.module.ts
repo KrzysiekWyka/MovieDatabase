744b1167426d7193161e1389ce51b7b8a42a559f
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 19:13:31 +0100

    Import UserModel

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { UserModel } from './user.model';
 
 @Module({
+  imports: [TypegooseModule.forFeature([UserModel])],
   providers: [UsersService],
   exports: [UsersService]
 })
