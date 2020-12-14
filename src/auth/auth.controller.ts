bad5365fb7de8446e7ae339b5c83c5771f8cdce5
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 21:21:43 +0100

    Register & describe auth/login action

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthLoginDto, AuthLoginResponseDto } from './auth-login.dto';
+import { UnauthorizedDto } from '../common/unauthorized.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ description: 'Login' })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Generated token', type: AuthLoginResponseDto })
+  @ApiUnauthorizedResponse({
+    description: 'Username or password are incorrect',
+    type: UnauthorizedDto,
+  })
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Request() req) {
+  async login(@Request() req, @Body() loginDto: AuthLoginDto) {
     return this.authService.login(req.user);
   }
 }
