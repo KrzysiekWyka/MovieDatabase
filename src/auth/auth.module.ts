472b5414367f0c6dbdf252d2f3e0e20d45c91859
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-14 21:09:36 +0100

    Import ConfigModule & JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import authConfig from './auth.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PassportModule, forwardRef(() => UsersModule)],
+  imports: [
+    PassportModule,
+    ConfigModule.forFeature(authConfig),
+    JwtModule.register({}),
+    forwardRef(() => UsersModule),
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
