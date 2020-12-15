b6c3f932ff537216dd50baa994b96e0b55435b67
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-15 22:25:15 +0100

    Create MovieService & tests

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,18 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { OmdbService } from '../omdb/omdb.service';
+import { MoviesRepository } from './movies.repository';
+import { Provider } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { when } from 'jest-when';
+import * as _ from 'lodash';
 
 describe('MoviesService', () => {
-  let service: MoviesService;
+  let usersService: Partial<Record<keyof UsersService, jest.Mock>>;
+  let moviesRepository: Partial<Record<keyof MoviesRepository, jest.Mock>>;
+  let omdbService: Partial<Record<keyof OmdbService, jest.Mock>>;
+  let sut: MoviesService;
 
   beforeEach(async () => {
+    const fakeUsersService: Provider = {
+      provide: UsersService,
+      useValue: {
+        checkUserLimitOrThrows: jest.fn(),
+        incrementAndResetUserLimitOrThrows: jest.fn(),
+      },
+    };
+
+    const fakeMoviesRepository: Provider = {
+      provide: MoviesRepository,
+      useValue: { findOneByTitle: jest.fn(), createOrUpdate: jest.fn() },
+    };
+
+    const fakeOmdbService: Provider = {
+      provide: OmdbService,
+      useValue: { getMovieInfoByTitle: jest.fn() },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
+      providers: [
+        MoviesService,
+        fakeMoviesRepository,
+        fakeOmdbService,
+        fakeUsersService,
+      ],
     }).compile();
 
-    service = module.get<MoviesService>(MoviesService);
+    sut = module.get<MoviesService>(MoviesService);
+    moviesRepository = module.get<MoviesRepository, unknown>(MoviesRepository);
+    omdbService = module.get<OmdbService, unknown>(OmdbService);
+    usersService = module.get<UsersService, unknown>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createMovie', () => {
+    const title = 'fooBar';
+    const creatorId = 'creator123';
+
+    it('should return existing movie when specified movie already exists', async () => {
+      const existingMovie = {
+        title: title,
+        _id: 'movie123',
+      };
+
+      when(moviesRepository.findOneByTitle)
+        .calledWith(title)
+        .mockResolvedValueOnce(existingMovie);
+
+      const result = await sut.createMovie(title, creatorId);
+
+      expect(result).toBe(existingMovie);
+      expect(usersService.checkUserLimitOrThrows).toHaveBeenCalledWith(
+        creatorId,
+      );
+      expect(omdbService.getMovieInfoByTitle).not.toHaveBeenCalled();
+    });
+
+    it('should get info and create movie', async () => {
+      const movieInfo = {
+        Title: title,
+        Genre: 'genre123',
+        Directory: 'directory123',
+        Released: new Date().getTime(),
+      };
+
+      const insertedMovie = {
+        title: title,
+        genre: movieInfo.Genre,
+        directory: movieInfo.Directory,
+        releasedDate: new Date(movieInfo.Released),
+        _id: 'movie123',
+      };
+
+      when(omdbService.getMovieInfoByTitle)
+        .calledWith(title)
+        .mockResolvedValueOnce({ ...movieInfo, foo: 'bar' });
+
+      when(moviesRepository.createOrUpdate)
+        .calledWith(_.omit(insertedMovie, '_id'))
+        .mockResolvedValueOnce(insertedMovie);
+
+      const result = await sut.createMovie(title, creatorId);
+
+      expect(result).toBe(insertedMovie);
+      expect(
+        usersService.incrementAndResetUserLimitOrThrows,
+      ).toHaveBeenCalledWith(creatorId);
+    });
   });
 });
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { OmdbService } from '../omdb/omdb.service';
+import { MoviesRepository } from './movies.repository';
+import * as _ from 'lodash';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    private readonly omdbService: OmdbService,
+    private readonly moviesRepository: MoviesRepository,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async createMovie(title: string, creatorId: string) {
+    await this.usersService.checkUserLimitOrThrows(creatorId);
+
+    const existingMovie = await this.moviesRepository.findOneByTitle(title);
+
+    if (!_.isEmpty(existingMovie)) {
+      return existingMovie;
+    }
+
+    return this.fetchDataAndUpsert(title, creatorId);
+  }
+
+  private async fetchDataAndUpsert(title: string, creatorId: string) {
+    const result = await this.omdbService.getMovieInfoByTitle(title);
+
+    await this.usersService.incrementAndResetUserLimitOrThrows(creatorId);
+
+    return this.moviesRepository.createOrUpdate({
+      title: result.Title,
+      genre: result.Genre,
+      directory: result.Directory,
+      releasedDate: new Date(result.Released),
+    });
+  }
+}
