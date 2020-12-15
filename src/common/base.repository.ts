305e7a1a051b5bfb3c5576fffe0642cd7f138e31
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-15 22:26:07 +0100

    Fix mongoose import

--- src/common/base.repository.ts
+++ src/common/base.repository.ts
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { AnyParamConstructor } from '@typegoose/typegoose/lib/types';
+import { Types } from 'mongoose';
 
 export abstract class BaseRepository<T> {
   public static readonly UNIQUE_INDEX_ERROR_CODE = 11000;
@@ -16,7 +16,7 @@ export abstract class BaseRepository<T> {
     );
   }
 
-  static toObjectId(id: string | mongoose.Types.ObjectId) {
-    return typeof id === 'string' ? new mongoose.Types.ObjectId(id) : id;
+  static toObjectId(id: string | Types.ObjectId) {
+    return typeof id === 'string' ? new Types.ObjectId(id) : id;
   }
 }
