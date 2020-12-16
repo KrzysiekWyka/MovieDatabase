4285b66dae17b1cdff7e8de2fb3ced8378a9fc6c
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-16 18:17:00 +0100

    Import ConfigModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,14 @@ import { UsersService } from './users.service';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { UsersRepository } from './users.repository';
+import { ConfigModule } from '@nestjs/config';
+import usersConfig from './users.config';
 
 @Module({
-  imports: [TypegooseModule.forFeature([UserModel])],
+  imports: [
+    TypegooseModule.forFeature([UserModel]),
+    ConfigModule.forFeature(usersConfig),
+  ],
   providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository],
 })
