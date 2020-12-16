da3e08311bdcd4ac966a78d9c640a7612b268703
Krzysiek Wyka <krzysztof@wyka.dev>
2020-12-16 18:58:36 +0100

    Add Config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,23 @@ import { OmdbModule } from './omdb/omdb.module';
 import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
-// TODO: Add config validation
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGODB_URI: Joi.string()
+          .uri()
+          .required(),
+        JWT_SECRET: Joi.string().required(),
+        OMDB_API_KEY: Joi.string().required(),
+      }),
+    }),
     databaseProvider,
     OmdbModule,
     MoviesModule,
